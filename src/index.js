b3240b72393f5546cb2f4226bd68ceff89d60e2d
Francisco Javier Galán Guerrero <galan.guererro@bootcamp-upgrade.com>
2023-08-25 19:25:47 +0200

    fixed for deploy

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import { Server as WebsocketServer } from "socket.io";
 import http from "http";
 import { connectDB } from "./db";
 import sockets from "./sockets";
+import { PORT } from "./config";
 
 connectDB();
 const server = http.createServer(app);//hay que pasarlo a server http con la clase http
-const httpServer = server.listen(3000);//el servidor escucha en este puerto
+const httpServer = server.listen(PORT);//el servidor escucha en este puerto
 const io = new WebsocketServer(httpServer);// y le paso el servido a websocket service que va a poder servir el contenido de los sockets, hacer conecxiones
 sockets(io);
 
-console.log('server runing');
+console.log('server runing on port', PORT);
